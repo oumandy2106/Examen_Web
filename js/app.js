4af081c153f70c63e6c889973a7e317b0f4a7581
oumandy2106 <20243ds039@utez.edu.mx>
2026-02-28 01:34:17 -0600

    App.js corregida, solo se le añadio un for each como el de cocteles

--- js/app.js
+++ js/app.js
@@ -22,27 +22,33 @@ const getNoticias = async () =>{
         console.log("Ingresa una fecha");
         return;
     }
+
     const respuesta = await fetch(`https://newsapi.org/v2/everything?q=${inputBuscar}&from=${fromInput}&to=${toInput}&sortBy=popularity&apiKey=b882286be1dd4a4b9598d71925673c5e`);
     const datos = await respuesta.json();
+    console.log(datos);
+
+    
+    datos.articles.forEach(noticia => {
 
         const divX = document.createElement("div");
         divX.classList.add("col-md-6");
 
         divX.innerHTML = `<br>
         <div class="card" style="width: 18rem;">
-            <h2 class="card-title">${datos.title}</h2>
-            <img src="${datos.urlToImage}" class="card-img-top" alt="...">
+            <h2 class="card-title">${noticia.title}</h2>
+            <img src="${noticia.urlToImage}" class="card-img-top" alt="...">
             <div class="card-body">
-            <p class="card-text">${datos.description}</p>
-            <h5 class="card-text">${datos.source.name}</h5>
-            <h6 class="card-text">${datos.publishedAt}</h6>
-            <a href="${datos.url}" class="btn btn-primary">Go somewhere</a>
+            <p class="card-text">${noticia.description}</p>
+            <h5 class="card-text">${noticia.source.name}</h5>
+            <h6 class="card-text">${noticia.publishedAt}</h6>
+            <a href="${noticia.url}" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>
         <br>`
-        cardNoticias.appendChild(divX);
-
-}
+        cardNoticias.appendChild(divX);    
+    });
+    
+} 
 
 btnBuscar.addEventListener("click", getNoticias);
 
